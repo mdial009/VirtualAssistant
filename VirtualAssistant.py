5ed13244ea7e2414cb0dbe07b472e3d29f54083a
Madany Diallo <madanydiallo@Madanys-MacBook-Pro.local>
2020-05-26 21:38:23 -0400

    Initial Commit

---
+++ VirtualAssistant.py
@@ -0,0 +1,52 @@
+# Description: This is a Virtual Assistant program that gets the date, current time,
+# Respond back with a random greeeting, and return information on a person.
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# Ingores any warning messages
+import pyaudio
+import speech_recognition as sr
+import os
+import datetime
+import warnings
+import calendar
+import random
+import wikipedia
+from gtts import gTTS
+
+# Ingores any warning messages
+warnings.filterwarnings("ignore")
+
+# A fuction thats going to records audio and return it as a string
+
+
+def recordAudio():
+
+    # Record the audio
+    r = sr.Recognizer()  # Creating a recognizer object
+
+    # Open the microphone and start recording
+    with sr.Microphone() as source:
+        print("Say Something!")
+        audio = r.listen(source)
+
+    # Use Google Speech Recognition
+    data = " "
+    try:
+        data = r.recognize_google(audio)
+        print("You Said: "+data)
+    except sr.UnknownValueError:  # Checks for a error
+        print("Google Speech Recognition Could Not Understand You")
+    except sr.RequestError as e:
+        print("Request Results from Google Speech Recognition Service Error" + e)
+
+    return data
+
+
+recordAudio()
